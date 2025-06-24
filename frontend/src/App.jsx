bbb7bd9ea75655b69837fec55ba08966fc6db2ac
Sourav <souravbhardwaj997@gmail.com>
2025-06-24 10:51:55 +0530

    changed home route to index

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -88,7 +88,7 @@ function App() {
           </Route>
 
           <Route path="/" element={<Master />}>
-            <Route path="/" element={<Home />}></Route>
+            <Route index element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
             <Route path="/view-meals" element={<ViewMeals />}></Route>
             <Route
